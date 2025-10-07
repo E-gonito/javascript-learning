9662fd542daaf647b6aee97e9d6187177502cd50
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-10-07 23:18:59 +0100

    feat(closures): add example for javascript closure

--- eloquent_javascript.js
+++ eloquent_javascript.js
@@ -1,7 +1,24 @@
+// Chapter 1
 let total = 0;
 count = 1;
 while (count <= 10) {
-    total += count;
-    count += 1;
+  total += count;
+  count += 1;
 }
-console.log(total);
+//console.log(total);
+
+// Chapter 3 Closures
+const makeCounter = () => {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+};
+
+counter1 = makeCounter();
+counter2 = makeCounter();
+
+console.log(counter1());
+console.log(counter1());
+console.log(counter2());

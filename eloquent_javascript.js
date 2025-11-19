8878dc5e7bb699ca7b21ad15dd1e524a20681a39
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-11-19 18:26:23 +0000

    feat(files): Add pre-exisiting practice JS files

--- eloquent_javascript.js
+++ eloquent_javascript.js
@@ -91,4 +91,20 @@ try {
 } catch (error) {
   console.error(error.message); // Division by zero is not allowed.
 }
+
+function addTwoNums(a,b) {
+    try {
+        if(typeof(a) != 'number') {
+            throw new ReferenceError('the first argument is not a number')
+        } else if (typeof(b) != 'number') {
+            throw new ReferenceError('the second argument is not a number')
+        } else {
+            console.log(a + b)
+        }
+    } catch(err) {
+        console.log("Error!", err)
+    }
+}
+addTwoNums(5, "5")
+console.log("It still works")
 */

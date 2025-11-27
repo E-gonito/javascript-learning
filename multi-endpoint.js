46a55805f70f70808abe7ac7ad8c90776dc4a431
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-11-27 19:35:49 +0000

    feat(multi-endpoint): Complete implementation

--- multi-endpoint.js
+++ multi-endpoint.js
@@ -24,4 +24,69 @@ async function fetchWithRetry(url, maxRetries = 3) {
   }
 }
 
+async function fetchAllData() {
+  const BASE = "https://jsonplaceholder.typicode.com";
+  const response = await Promise.allSettled([
+    fetchWithRetry(`${BASE}/users`),
+    fetchWithRetry(`${BASE}/posts`),
+    fetchWithRetry(`${BASE}/comments`),
+  ]);
 
+  let users, posts, comments;
+  if (response[0].status === "fulfilled") {
+    users = response[0].value;
+  } else {
+    throw new Error("Failed to fetch users");
+  }
+  if (response[1].status === "fulfilled") {
+    posts = response[1].value;
+  } else {
+    throw new Error("Failed to fetch posts");
+  }
+  if (response[2].status === "fulfilled") {
+    comments = response[2].value;
+  } else {
+    comments = [];
+  }
+  return { users, posts, comments };
+}
+
+const enrichPosts = (data) => {
+  const users = data.users;
+  const posts = data.posts;
+  const comments = data.comments;
+  const findNameFromId = (users, id) => {
+    return users.find((user) => user.id === id).name;
+  };
+  const countCommentsForPost = (comments, postId) => {
+    return comments.reduce(
+      (total, comment) => (comment.postId === postId ? total + 1 : total),
+      0
+    );
+  };
+  const enrichedPosts = posts.map((post) => {
+    return {
+      ...post,
+      authorName: findNameFromId(users, post.userId),
+      commentCount: countCommentsForPost(comments, post.id),
+    };
+  });
+  return enrichedPosts;
+};
+
+const main = async () => {
+  try {
+    console.log("Fetching Data...");
+    const data = await fetchAllData();
+
+    console.log("Enriching Posts...");
+    const enrichedPosts = enrichPosts(data);
+
+    console.log("First enriched post:", enrichedPosts[0]);
+    console.log(`Total posts: ${enrichedPosts.length}`);
+  } catch (error) {
+    console.error(`Error!: ${error}`);
+  }
+};
+
+main();

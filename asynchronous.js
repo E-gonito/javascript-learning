f201d6cd31f675e519c00d6b67aca9c8ef8745f1
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-11-27 10:37:38 +0000

    feat(async): Add exercise 2 answer

--- asynchronous.js
+++ asynchronous.js
@@ -7,3 +7,17 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
     console.log(user.name); // This will error
   })
   .catch((error) => console.log("Error:", error.message));
+
+// Exercise 2
+Promise.resolve(5)
+  .then((num) => {
+    console.log("Step 1:", num);
+    return num * 2; // ✓ Now it returns 10
+  })
+  .then((num) => {
+    console.log("Step 2:", num); // Gets 10
+    return num + 10; // Returns 20
+  })
+  .then((num) => {
+    console.log("Step 3:", num); // Gets 20
+  });
